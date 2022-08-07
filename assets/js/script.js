86750bd9b6aaabc1409f6a3fdc4e37555f5f58f1
ChucktownTiger <clemsonalum07@gmail.com>
2022-08-07 11:23:50 -0400

    more work on java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // define variables of document elements
-var searchInputEl = document.getElementById("cityinput")
-var searchFormEl = document.getElementById("cityinputform")
+var cityInputEl = document.getElementById("cityinput")
+var cityInputFormEl = document.getElementById("cityinputform")
 var searchBtn = document.getElementById("searchbtn")
-var searchHistory = JSON.parse(localStorage.getItem("searchName")) || []
+var searchName = JSON.parse(localStorage.getItem("searchName")) || []
 var searchHistoryList = document.getElementById("history-list")
 var todaysWeatherEl = document.getElementById("todayweather")
 var cityDateEl = document.getElementById("city-date")
@@ -12,7 +12,7 @@ var windEl = document.getElementById("wind")
 var humidityEl = document.getElementById("humidity")
 var UVIndexEl = document.getElementById("UV-index")
 var weatherDisplayEl = document.querySelector(".weather-results")
-var historyForecastDisplayEl = document.querySelector("history-forecast")
+var fivedayEl = document.querySelector(".five-day-forecast")
 var d1El = document.getElementById("d1")
 var d2El = document.getElementById("d2")
 var d3El = document.getElementById ("d3")
@@ -21,14 +21,12 @@ var d5El = document.getElementById ("d5")
 
 var getWeatherData = function(userSearch) {
 
-    // assigning my created API Key on website to a constant
-const APIKey = "f2adfc525cc3c6c9a79c91dd777a0caa";
+    // my API key from the website
+    const APIKey = "f2adfc525cc3c6c9a79c91dd777a0caa";
    
-    // set variable to API depending on what city is search
-
     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + userSearch + "&APPID=" + APIKey;
 
-    // fetch request from weather API 
+    // fetch from API
     fetch(queryUrl)
     .then(response => { 
         if (!response.ok) { 
@@ -36,22 +34,22 @@ const APIKey = "f2adfc525cc3c6c9a79c91dd777a0caa";
             errorDisplay.setAttribute("class", "card-body")
             errorDisplay.setAttribute("class", "text-center")
             errorDisplay.textContent = "Invalid City Name"
-            searchFormEl.reset()
+            cityInputFormEl.reset()
          } else {
          return response.json()
          }
     })
     .then(data => {
-        if (!searchHistory.includes(data.name)) {
-            searchHistory.push(data.name)
-            localStorage.setItem("searchName", JSON.stringify(searchHistory))
+        if (!searchName.includes(data.name)) {
+            searchName.push(data.name)
+            localStorage.setItem("searchName", JSON.stringify(searchName))
             searchHistoryDisplay()
-            searchFormEl.reset()
+            cityInputFormEl.reset()
             }
 
-            // remove css style display:none
+        // un hide hidden elements
         weatherDisplayEl.classList.remove("weather-results")
-        // historyForecastDisplayEl.classList.remove("history-forecast")
+        fivedayEl.classList.remove("five-day-forecast")
 
         // display data on elements
 
@@ -62,36 +60,28 @@ const APIKey = "f2adfc525cc3c6c9a79c91dd777a0caa";
         let year = currentDate.getFullYear()
         todaysWeatherEl.textContent = data.name + " " + month + "/" + date + "/"  + year
 
-        // retrieve icon dependent on current weather
           let weatherIcon = data.weather[0].icon
             weatherIconImg = document.createElement("img")
             weatherIconImg.setAttribute("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
             todaysWeatherEl.append(weatherIconImg)
 
-        // display current temp
            tempEl.innerHTML = "Temperature:" + " " + data.main.temp + " " + '\u00B0F'
 
-           // display feels like temp
            feelsLikeTempEl.innerHTML = "Feels Like:" + " " + data.main.feels_like + " " + '\u00B0F'
 
-        // display current wind mph
             windEl.innerHTML= "Wind Speed:" + " " + data.wind.speed + "mph"
 
-        // display current humidity %
             humidityEl.innerHTML = "Humidity:" + " " + data.main.humidity + "%"
 
-        // display current uv-index
-
-            // define latitude and longitude because those are required to get the UV value
             var lat = data.coord.lat;
             var lon = data.coord.lon;
             var UVQueryUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&APPID=" + APIKey;
-            // fetch request from weather API 
+            
+            
             fetch(UVQueryUrl)
             .then(response => response.json())
             .then(data => {
-    
-         //  color-code the uv index values dependent on value
+
                 var UVIndexValue = document.createElement("span")           
                 if (data[0].value <=2) {
                     UVIndexEl.setAttribute("class", "badge badge-success")
@@ -113,32 +103,28 @@ const APIKey = "f2adfc525cc3c6c9a79c91dd777a0caa";
         .then (data => {
             console.log(data)  
             
-            // create an array for days in forecast
+            // create forecast array
 
-            var forecastArray = [d1l, d2El, d3El, d4El, d5El]
+            var forecastArray = [d1El, d2El, d3El, d4El, d5El]
             var k=0
             
             for (let i = 1; i < 8; i++) {           
-            // create elements for each weather variable
             var dateTitle = document.createElement("h4")
             var tempP = document.createElement("p")
             var feelsLikeP = document.createElement("p")
             var windP = document.createElement("p")
             var humidityP = document.createElement("p")
 
-            // setting date display to array value
             let iDate = new Date(data.daily[i].dt *1000)
-           let idate = iDate.getDate()
-           let imonth = iDate.getMonth()
-           let iyear = iDate.getFullYear()
-           dateTitle.innerHTML= imonth + "/" + idate + "/"  + iyear
+            let idate = iDate.getDate()
+            let imonth = iDate.getMonth()
+            let iyear = iDate.getFullYear()
+            dateTitle.innerHTML= imonth + "/" + idate + "/"  + iyear
 
-           // display weather icon
            let iWeatherIcon = data.daily[i].weather.icon
            iWeatherIconImg = document.createElement("img")
            iWeatherIconImg.setAttribute("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
          
-            // setting values for the weather variables
            tempP.innerHTML = "Temp:" + " " + data.daily[i].temp.day + '\u00B0F'
            feelsLikeP.innerHTML = "Feels Like:" + " " + data.daily[i].feels_like.day + '\u00B0F'
            windP.innerHTML = "Wind:" + " " + data.daily[i].wind_speed + "mph"
@@ -151,14 +137,14 @@ const APIKey = "f2adfc525cc3c6c9a79c91dd777a0caa";
           forecastArray[k].append(feelsLikeP)
           forecastArray[k].append(windP)
           forecastArray[k].append(humidityP)
-          j++
+          k++
         }
         })
     }) 
 }
 
 searchBtn.addEventListener("click", function() { 
-    userSearch = searchInputEl.value
+    userSearch = cityInputEl.value
     getWeatherData(userSearch);
 })
 
@@ -168,11 +154,11 @@ function searchHistoryDisplay () {
 
     // for loop to create a list of city searches
     searchHistoryList.innerHTML = "";
-    for (let i = 0; i < searchHistory.length; i++) {
+    for (let i = 0; i < searchName.length; i++) {
          var searchHistoryItem = document.createElement("button")
         searchHistoryItem.className = "btn btn-light btn-outline-dark btn-xs btn-block" 
-        searchHistoryItem.innerHTML = searchHistory[i]
-        searchHistoryItem.setAttribute("value", searchHistory[i])
+        searchHistoryItem.innerHTML = searchName[i]
+        searchHistoryItem.setAttribute("value", searchName[i])
         searchHistoryList.append(searchHistoryItem)
 
         // be able to click a city in the search history and have its data populate on the page
